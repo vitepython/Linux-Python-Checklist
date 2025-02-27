859cbf09220dddb17b2703b45223bfbeb9cfd00a
Pensando Panama <vitepython@users.noreply.github.com>
2025-02-27 01:19:47 -0500

    Update checklist.py
    Key Changes:
    Shebang Line: Updated to #!/usr/bin/env python3 to ensure the script uses Python 3.
    
    Print Statements: Converted to the print() function.
    
    Raw Input: Replaced raw_input with input for Python 3.
    
    Exception Handling: Updated the answer() function to properly handle the user input and global variable.

--- checklist.py
+++ checklist.py
@@ -1,7 +1,8 @@
-print '''
-Python Checklist version 0.01
-Pensando Panama Copyright 2013
+#!/usr/bin/env python3
 
+print('''
+Python Checklist version 0.02
+Pensando Panama Copyright 2025
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -15,77 +16,75 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+''')
 
-prompt = raw_input("PRESS ENTER ANY KEY TO CONTINUE or CNTR^C TO QUIT")
+input("PRESS ENTER ANY KEY TO CONTINUE or CTRL+C TO QUIT")
 
-print "Please type yes or no"
+print("Please type yes or no")
 
 def answer():
-    if  p is not ("yes", "no"):
-        print "Please read instructions again"
-        
-    if  p == "yes":
-        print "Good JOB"
-           
-    else:
-        p == "no" 
-        print "START OVER!!!"
+    global p
+    if p not in ("yes", "no"):
+        print("Please read instructions again")
+    elif p == "yes":
+        print("Good JOB")
+    elif p == "no":
+        print("START OVER!!!")
         quit()
 
-print "*****"            
-print "Did you start your function definition with def?"
-p = raw_input('> ')
+print("*****")
+print("Did you start your function definition with def?")
+p = input('> ')
 answer()
 
-print "Does your function name have only characters and _ (underscore) characters?"
-p = raw_input('> ')
+print("Does your function name have only characters and _ (underscore) characters?")
+p = input('> ')
 answer()
 
-print "Did you put an open parenthesis ( right after the function name?"
-p = raw_input('> ')
+print("Did you put an open parenthesis ( right after the function name?")
+p = input('> ')
 answer()
 
-print "Did you put your arguments after the parenthesis ( separated by commas?"
-p = raw_input('> ')
+print("Did you put your arguments after the parenthesis ( separated by commas?")
+p = input('> ')
 answer()
 
-print "Did you make each argument unique (meaning no duplicated names)?"
-p = raw_input('> ')
+print("Did you make each argument unique (meaning no duplicated names)?")
+p = input('> ')
 answer()
 
-print "Did you put a close parenthesis and a colon ): after the arguments?"
-p = raw_input('> ')
+print("Did you put a close parenthesis and a colon ): after the arguments?")
+p = input('> ')
 answer()
 
-print "Did you indent all lines of code you want in the function four spaces? No more, no less."
-p = raw_input('> ')
+print("Did you indent all lines of code you want in the function four spaces? No more, no less.")
+p = input('> ')
 answer()
 
-print "Did you 'end' your function by going back to writing with no indent (dedenting we call it)?"
-p = raw_input('> ')
+print("Did you 'end' your function by going back to writing with no indent (dedenting we call it)?")
+p = input('> ')
 answer()
 
-print "And when you run ('use' or 'call') a function, check these things:"
-p = raw_input('> ')
+print("And when you run ('use' or 'call') a function, check these things:")
+p = input('> ')
 answer()
 
-print "Did you call/use/run this function by typing its name?"
-p = raw_input('> ')
+print("Did you call/use/run this function by typing its name?")
+p = input('> ')
 answer()
 
-print "Did you put the ( character after the name to run it?"
-p = raw_input('> ')
+print("Did you put the ( character after the name to run it?")
+p = input('> ')
 answer()
 
-print "Did you put the values you want into the parenthesis separated by commas?"
-p = raw_input('> ')
+print("Did you put the values you want into the parenthesis separated by commas?")
+p = input('> ')
 answer()
 
-print "Did you end the function call with a ) character?"
-p = raw_input('> ')
+print("Did you end the function call with a ) character?")
+p = input('> ')
 answer()
 
-print "*****"
-print "You have finished debugging"
+print("*****")
+print("You have finished debugging")
 quit()
